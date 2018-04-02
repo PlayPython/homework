fd95ac2815b2d8bcd26ae59b472dbe63ac921c7d
yanzi5709 <xinyuly@163.com>
2018-04-02 16:20:58 +0800

    try again

--- yanzi/yanzi.py
+++ yanzi/yanzi.py
@@ -1,4 +1,4 @@
-
+'''author yanzi'''
 #数值类型练习
 #- 使用循环和算数运算，生成0到20的偶数
 #- 使用循环和算数运算，生成0到20的奇数
@@ -14,19 +14,19 @@ def fun(n):
     #return(a,b)
     print("0到20偶数是{0}".format(a))
     print("0到20奇数是{0}".format(b))
-if __name__ == '__main__':
-    fun(20)
+
+fun(20)
 
 #- 写一个函数，判断一个数是否被另外一个数整除
 def sub(m,n):
     if m%n==0:
-         print("m能被n整除")
+         print("{0}能被{1}整除".format(m,n))
     else:
-         print("m不能被n整除")
+         print("{0}不能被{1}整除".format(m,n))
 
 
-if __name__ == '__main__':
-    sub(2,3)
+sub(2,3)
+sub(40,5)
 
 #- 生成一个有N个元素的由随机数n组成的列表，其中N和n的取值范围分别为（1< N
 #<= 100）和（0<=n<=2**31 -1）。然后再随机从这个列表中取N（1<=N<=100）个
@@ -41,9 +41,8 @@ import random
 def drop(n):
     print("去掉前后空格的字符串为：{0}".format(n.strip()))
 
-if __name__ == '__main__':
-    a=drop(" abc ")
-
+drop("#  asdggd  ")
+drop("  sda ddgg ")
 
 
 #- 输入一个字符串，判断是否为回文
@@ -53,11 +52,12 @@ def fun1(n):
         if n[i]!=n[-1-i]: #理解回文字符串就是正向和反向排序都是一样的。
             num=num+1  #通过计数的方式，当相应位置对应相等时计数1次
     if num==0:
-        print("这是一个回文字符串")
+        print("{0}是一个回文字符串".format(n))
     else:
-        print("这不是一个回文字符串")
-if __name__ == '__main__':
-    fun1("abccba")
+        print("{0}不是一个回文字符串".format(n))
+
+fun1("abccba")
+fun1("dvdv")
 
 #- 给出一个整型值，能够返回该值得英文，例如输入89，返回eighty-nine，限定值在0~100
 
@@ -86,16 +86,21 @@ if __name__ == '__main__':
 #- 写一个函数，返回N的阶乘
 def jiecheng(n):
     N=1  #1乘以任何数都为任何数本身，对结果没影响故赋了初始值1
-    if isinstance(n,int): #阶乘是针对正整数而言的，规定0的阶乘为1，故多了一个判断
+    if isinstance(n,int) and n>=0: #阶乘是针对正整数而言的，规定0的阶乘为1，故多了一个判断
         for i in range(1,n+1):
             if n==0:
                 N=1
             else:
                 N=N*i #相当于依次乘到数本身
-        print("这个数的阶乘是{0}".format(N))
+        print("{0}的阶乘是{1}".format(n,N))
         return(N)  #这个地方输出和返回的位置很重要
-if __name__ == '__main__':
-    jiecheng(4)
+    else:
+        print("{0}不存在阶乘".format(n))
+
+
+jiecheng(4)
+jiecheng(0)
+jiecheng(-200)
 
 
 #- 判断一个数字是否为素数
@@ -112,10 +117,9 @@ def sushu(n):
     else:
         print("{0}不是一个素数".format(n))
 
-if __name__ == '__main__':
-    sushu(2)
-    sushu(1)
-    sushu(3.3)
+sushu(2)
+sushu(1)
+sushu(3.3)
 #- 统计一个文本中每个单词出现个数
 
 
