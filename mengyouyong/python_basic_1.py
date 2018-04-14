e43a2bba63bd99575e89d4cbf9a55506a412fac9
PC-201710222231\Administrator <15682053765@163.com>
2018-04-14 22:45:04 +0800

    文件、条件、字典和集合

--- mengyouyong/python_basic_1.py
+++ mengyouyong/python_basic_1.py
@@ -1,6 +1,7 @@
 import random
 import math
 import time
+import collections
 
 # 打印π
 print(math.pi)
@@ -117,11 +118,107 @@ def Comput_Date(date1, date2):
     else:
         print("日期差为{0}天".format(-y))
 
-Comput_Date("2018-4-8","2018-4-3")
+
+Comput_Date("2018-4-8", "2018-4-3")
 
 '''
 #字典和集合练习
 - 创建一个字典，把字典中的键按照字母顺序显示出来
 - 根据已经排序好的字母的键，显示这个字典中的键和值
-- 使用随机数模块生成一个随机数集合A和B，每个集合是0到9的随机数集合。生成之后显示结果A|B和A&B
 '''
+
+
+def Dict_Key_Sort(dict1):
+    sort_key = sorted(dict1.keys())
+    print(sort_key)  # 创建一个字典，把字典中的键按照字母顺序显示出来
+    li = sort_key.copy()
+    dict2 = {}
+    for i in li:
+        sort_key.append(dict1.get(i))  # 将字典的键和值组成一个列表
+    for j in range(len(li)):
+        dict2[sort_key[j]] = sort_key[j + len(li)]  # 通过生成新字典的方法将原字典按照已排序的键进行重新组合
+    print(dict2)  # 根据已经排序好的字母的键，显示这个字典中的键和值
+
+
+Dict_Key_Sort({"H": "我是H的值", "D": "我是D的值", "S": "我是S的值", "A": "我是A的值", "F": "我是F的值", "B": "我是B的值"})
+
+
+# - 使用随机数模块生成一个随机数集合A和B，每个集合是0到9的随机数集合。生成之后显示结果A|B和A&B
+def Random_Number():
+    a = random.randrange(0, 9, 1)
+    b = random.randrange(0, 9, 1)
+    print(a | b, a & b)
+
+
+Random_Number()
+
+"""
+#文件和条件语句练习
+- 文件过滤，显示一个文件的所有行，忽略以#开始的行
+- 比较文件，写一个比较文件的程序，如果不同，给出第一个不同处的行号和列号
+- 写一个函数，返回N的阶乘
+- 判断一个数字是否为素数
+- 统计一个文本中每个单词出现个数
+
+"""
+
+# - 文件过滤，显示一个文件的所有行，忽略以#开始的行
+with open("./test.txt", "r") as f:
+    for eachline in f.readlines():
+        if str(eachline)[0] == "#":
+            continue
+        else:
+            print(eachline, end='')
+
+
+# - 写一个函数，返回N的阶乘
+def Return_Factorial(N):
+    sum = 1
+    for i in range(1, N + 1):
+        sum = i * sum
+    print(sum)
+
+
+Return_Factorial(7)
+
+
+# - 判断一个数字是否为素数
+def Prime_Number(prime_number):
+    number1 = 0
+    for i in range(1, prime_number + 1):
+        if prime_number % i == 0:
+            number1 += 1
+        else:
+            pass
+    if number1 == 2:
+        print("{0}是素数".format(prime_number))
+    else:
+        print("{0}不是素数".format(prime_number))
+
+
+Prime_Number(17)
+
+# - 统计一个文本中每个单词出现个数
+with open('./test3.txt', 'r') as f:
+    str1 = f.read().split(' ')
+    dict12 = collections.Counter(str1)
+    for i in list(set(str1)):
+        print("{0}的出现的次数是{1}次".format(i, dict(dict12)[i]))
+
+import csv
+
+# - 比较文件，写一个比较文件的程序，如果不同，给出第一个不同处的行号和列号
+"""list_sum=[]
+with open('test3.csv', 'r',encoding='gbk') as f:
+    reader = csv.reader(f)
+    for i in reader:
+        list_sum.append(i)
+with open('test2.csv', 'r',encoding='gbk') as f:
+    reader = csv.reader(f)
+    for i in reader:
+        list_sum.append(i)
+for a in range(len(list_sum//2)):
+    if list_sum[a] != list_sum[a+len(list_sum//2)]:
+        print()
+
+"""
